77ad4980c9e6a3401f33b233813e78ae945f72f2
Venkatesh2007 <venkateshbadamdurga@gmail.com>
2025-05-28 11:57:38 +0530

    Import vibe-frontend into monorepo

---
+++ frontend/src/components/UI.jsx
@@ -0,0 +1,136 @@
+import { useRef, useState } from "react";
+import { useChat } from "../hooks/useChat";
+
+export const UI = ({ hidden, ...props }) => {
+  const input = useRef();
+  const { chat, loading, cameraZoomed, setCameraZoomed, message, isVIP, toggleVIPMode } = useChat();
+  const [listening, setListening] = useState(false);
+
+
+  const sendMessage = (text) => {
+    if (!loading && !message && text.trim()) {
+      chat(text);
+      input.current.value = "";
+    }
+  };
+
+  const startListening = () => {
+    const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+    recognition.lang = "en-US";
+    recognition.continuous = false;
+    recognition.interimResults = false;
+
+    recognition.onstart = () => setListening(true);
+    recognition.onend = () => setListening(false);
+
+    recognition.onresult = (event) => {
+      const transcript = event.results[0][0].transcript;
+      sendMessage(transcript);
+    };
+
+    recognition.start();
+  };
+
+
+  if (hidden) {
+    return null;
+  }
+
+  return (
+    <>
+      <div className="fixed top-0 left-0 right-0 bottom-0 z-10 flex justify-between p-4 flex-col pointer-events-none ">
+        <div className="self-start backdrop-blur-md bg-gradient-to-bl bg-opacity-50 p-4 rounded-lg">
+
+        </div>
+        <div className="w-full flex flex-col items-end justify-center gap-4">
+          <button
+            onClick={() => setCameraZoomed(!cameraZoomed)}
+            className="pointer-events-auto bg-yellow-500 hover:bg-yellow-600 text-white p-4 rounded-md"
+          >
+            {cameraZoomed ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM13.5 10.5h-6"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM10.5 7.5v6m3-3h-6"
+                />
+              </svg>
+            )}
+          </button>
+          <button
+            onClick={toggleVIPMode}
+            className="pointer-events-auto bg-yellow-500 hover:bg-yellow-600 text-white p-4 rounded-md"
+          >
+            {/* <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+              <path stroke="currentColor" stroke-linecap="square" stroke-linejoin="round" stroke-width="2" d="M10 19H5a1 1 0 0 1-1-1v-1a3 3 0 0 1 3-3h2m10 1a3 3 0 0 1-3 3m3-3a3 3 0 0 0-3-3m3 3h1m-4 3a3 3 0 0 1-3-3m3 3v1m-3-4a3 3 0 0 1 3-3m-3 3h-1m4-3v-1m-2.121 1.879-.707-.707m5.656 5.656-.707-.707m-4.242 0-.707.707m5.656-5.656-.707.707M12 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+            </svg> */}
+            {
+              !isVIP?
+              <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12h4m-2 2v-4M4 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+</svg>
+
+
+              :
+<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 12h4M4 18v-1a3 3 0 0 1 3-3h4a3 3 0 0 1 3 3v1a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1Zm8-10a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+</svg>
+
+            }
+
+
+          </button>
+        </div>
+        <div className="flex items-center gap-2 pointer-events-auto max-w-screen-sm w-full mx-auto">
+          <input
+            className="w-full placeholder:text-black placeholder:italic p-4 rounded-md bg-slate-100 bg-opacity-50 backdrop-blur-md text-black border border-yellow-500"
+            placeholder="Type a message..."
+            ref={input}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                sendMessage(input.current.value);
+              }
+            }}
+          />
+          <button
+            disabled={loading || message}
+            onClick={() => sendMessage(input.current.value)}
+            className={`bg-yellow-500 hover:bg-yellow-600 text-white p-4 px-10 font-semibold uppercase rounded-md ${loading || message ? "cursor-not-allowed opacity-30" : ""
+              }`}
+          >
+            Send
+          </button>
+          <button
+            onClick={startListening}
+            className={`bg-yellow-500 hover:bg-yellow-600 text-white p-2  rounded-md ${listening ? "opacity-50" : ""
+              }`}
+          >
+            {listening ? "Listening..." : "🎤 speak"}
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
